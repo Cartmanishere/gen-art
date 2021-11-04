71a04020db48514db80cdb3fd117b4a7a284f978
Cartmanishere <apbetahouse45@gmail.com>
2021-11-04 13:32:44 +0530

    Add balls under forces

--- src/gen_art/ball.clj
+++ src/gen_art/ball.clj
@@ -1,44 +1,127 @@
 (ns gen-art.ball
-  (:require [genartlib.util :as gu]
+  (:require [euclidean.math.vector :as v]
+            [genartlib.util :as gu]
             [gen-art.core :as core]
             [quil.core :as q]
             [quil.middleware :as m]))
 
+(defn rand-color
+  []
+  (doall (repeatedly 3 #(rand-int 256))))
+
+;;;;;;;;;;;;;;;;;;;;
+;; Ball Functions ;;
+;;;;;;;;;;;;;;;;;;;;
+
+(defn create-ball
+  "Create a ball object containing `location`, `veloctiy`, `acceleration` and `mass`."
+  ([]
+   (create-ball (rand-int (gu/w 0.5))
+                (rand-int (gu/h 0.5))))
+  ([x y]
+   (create-ball x y (+ 20 (rand-int 30))))
+  ([x y mass]
+   {:location (v/vector x y)
+    :mass mass
+    :color (rand-color)
+    :velocity (v/vector 0.0 0.0)
+    :acceleration (v/vector 0.0 0.0)}))
+
+(defn apply-force
+  "Apply multiple forces to the ball and calculate the acceleration based on that."
+  [{:keys [mass] :as ball} & forces]
+  (reduce (fn [ball f]
+            (update ball
+                    :acceleration
+                    #(v/add %
+                            (v/div f
+                                   (v/vector mass mass)))))
+          ball
+          forces))
+
+(defn update-ball
+  "Take a ball and update the location of the ball using the acceleration and velocity."
+  [{:keys [location velocity acceleration] :as ball}]
+  (let [new-velocity (v/add velocity acceleration)
+        new-location (v/add location new-velocity)]
+    (assoc ball
+           :acceleration (v/vector 0.0 0.0)
+           :velocity new-velocity
+           :location new-location)))
+
+(defn check-edge
+  "Whenever the edge hits an edge, loop around the canvas"
+  [{:keys [location] :as ball}]
+  (let [x (location 0)
+        y (location 1)]
+    (cond-> ball
+
+      ;; TODO: This is ugly. Fix it.
+      (or (> x (gu/w))
+          (< x 0))
+      ((fn [b]
+         (-> b
+             (update :velocity (fn [vc]
+                                 (v/mult vc (v/vector -1 1))))
+             (update :location (fn [loc]
+                                 (v/vector (if (> x (gu/w))
+                                             (gu/w)
+                                             0)
+                                           (loc 1)))))))
+
+
+      (or (> y (gu/h))
+          (< y 0))
+      ((fn [b]
+         (-> b
+          (update :velocity (fn [vc]
+                              (v/mult vc (v/vector 1 -1))))
+          (update :location (fn [loc]
+                              (v/vector (loc 0)
+                                        (if (> y (gu/h))
+                                          (gu/h)
+                                          0))))))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Animation Functions ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn update-state
+  [{:keys [balls xoff yoff] :as state}]
+  (let [force (v/vector (q/noise xoff) (q/noise yoff))
+        new-balls (->> balls
+                       (map #(apply-force % force))
+                       (map #(update-ball %))
+                       (mapv #(check-edge %)))]
+    (assoc state
+           :xoff (inc xoff)
+           :yoff (inc yoff)
+           :balls new-balls)))
+
+
 (defn setup []
   (q/frame-rate 120)
-  (q/color-mode :hsb)
-  (q/background 240)
-  {:location {:x 100 :y 100}
-   :velocity {:x 2.5 :y 0.5}})
+  {:balls (doall (repeatedly 100 create-ball))
+   :xoff 1
+   :yoff 1000000})
 
-(defn add-vec
-  [{x1 :x y1 :y} {x2 :x y2 :y}]
-  {:x (+ x1 x2)
-   :y (+ y1 y2)})
-
-(defn update-state [{:keys [location velocity]}]
-  (let [new-location (add-vec location velocity)
-        new-velocity {:x (if (or (> (:x new-location) (gu/w))
-                                 (< (:x new-location) 0))
-                           (* -1 (:x velocity))
-                           (:x velocity))
-                      :y (if (or (> (:y new-location) (gu/h))
-                                 (< (:y new-location) 0))
-                           (* -1 (:y velocity))
-                           (:y velocity))}]
-    {:location new-location
-     :velocity new-velocity}))
-
-(defn draw [{:keys [location]}]
+(defn draw [{:keys [balls]}]
   (q/stroke 0)
-  (q/fill 175)
-  (q/background 240)
-  (q/ellipse (:x location) (:y location) 32 32))
+  (q/background 0 0 0)
+  (doseq [{:keys [location color mass]} balls]
+    (apply q/fill color)
+    (q/ellipse (location 0)
+               (location 1)
+               mass
+               mass)))
 
-(def app-config {:title "Nature of code - Bouncing ball in vector"
-                 :size [200 360]
+
+(def app-config {:title "Ball under forces"
+                 :size [640 480]
                  :setup setup
                  :draw draw
                  :update update-state
                  :features [:keep-on-top]
                  :middleware [m/fun-mode]})
+
+(def app (core/create app-config))
